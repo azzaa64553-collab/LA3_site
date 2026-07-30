2b5b40e1498d818e3aa495c7549794780d415fc7
azzaa64553-collab <azzaa64553@gmail.com>
2026-07-30 17:47:54 +0330

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,100 +1,76 @@
-* {
-    box-sizing: border-box;
-    font-family: Tahoma, Arial, sans-serif;
+const adminPassword = "mrM_13900_1213";
+
+
+function loginAdmin(){
+
+let pass = document.getElementById("adminPass").value;
+
+
+if(pass === adminPassword){
+
+document.getElementById("login").style.display="none";
+document.getElementById("panel").style.display="block";
+
+loadUsers();
+
+
+}else{
+
+alert("رمز اشتباه است");
+
 }
 
-body {
-    margin: 0;
-    min-height: 100vh;
-    background: #000;
-    color: white;
-    direction: rtl;
 }
 
-/* پنل اصلی */
-.admin-box {
-    width: 90%;
-    max-width: 500px;
-    margin: 40px auto;
-    padding: 25px;
-    background: #050505;
-    border: 2px solid #00ff66;
-    border-radius: 25px;
-    box-shadow:
-        0 0 15px #00ff66,
-        0 0 35px rgba(0,255,102,0.5);
-}
 
-/* عنوان */
-h1 {
-    text-align: center;
-    color: #00ff66;
-    font-size: 35px;
-    text-shadow: 0 0 15px #00ff66;
-    margin-bottom: 30px;
-}
 
-/* ورودی ها */
-input {
-    width: 100%;
-    padding: 15px;
-    margin: 10px 0;
-    border-radius: 15px;
-    border: 2px solid #00ff66;
-    background: #111;
-    color: white;
-    font-size: 18px;
-    outline: none;
-}
+// فعلاً اطلاعات نمونه
+// بعد از وصل Firebase این بخش واقعی می‌شود
 
-input:focus {
-    box-shadow: 0 0 15px #00ff66;
-}
+function loadUsers(){
 
-/* دکمه */
-button {
-    width: 100%;
-    padding: 15px;
-    margin-top: 15px;
-    border: none;
-    border-radius: 15px;
-    background: #00ff66;
-    color: black;
-    font-size: 20px;
-    font-weight: bold;
-    cursor: pointer;
-    box-shadow: 0 0 20px #00ff66;
+let users = [
+{
+phone:"09123456789",
+code:"12345",
+date:"امروز"
 }
+];
 
-button:hover {
-    transform: scale(1.03);
+
+let table = document.getElementById("users");
+
+table.innerHTML="";
+
+
+users.forEach(user=>{
+
+
+let row = document.createElement("tr");
+
+
+row.innerHTML=`
+
+<td>${user.phone}</td>
+<td>${user.code}</td>
+<td>${user.date}</td>
+<td>
+<button class="delete-btn">
+حذف
+</button>
+</td>
+
+`;
+
+
+table.appendChild(row);
+
+
+});
+
+
+document.getElementById("count").innerHTML =
+"تعداد کاربران: " + users.length;
+
+
 }
-
-/* لیست اطلاعات */
-.user-card {
-    background: #080808;
-    border: 1px solid #00ff66;
-    border-radius: 15px;
-    padding: 15px;
-    margin-top: 15px;
-    box-shadow: 0 0 10px rgba(0,255,102,0.5);
-}
-
-.user-card span {
-    color: #00ff66;
-}
-
-/* دکمه حذف */
-.delete-btn {
-    background: red;
-    color: white;
-    box-shadow: 0 0 15px red;
-}
-
-/* پیام ها */
-.message {
-    text-align: center;
-    margin-top: 15px;
-    color: #00ff66;
-    font-size: 18px;
-      }
